01f57af38532b725fbfbce9d8238d8ed245f538a
lorenafigueredo <lorenafigueredo@892cf779-585f-cb9d-664d-18cd154d568d>
2010-10-26 16:37:42 +0000

    BD con Weka

--- trunk/src/my/gui/DataBase.java
+++ trunk/src/my/gui/DataBase.java
@@ -6,19 +6,38 @@
 package my.gui;
 import java.sql.*;
 
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import weka.core.Instances;
+import weka.experiment.InstanceQuery;
+
 
 public class DataBase {
     //Variables de Lore para la BD
     public static Connection connection;
+    InstanceQuery query;
     //Conectar a la BD
     public static void Conectar(){
         String cadenaconexion = "";
         String prueba = "jdbc:mysql://localhost/com?" + "user=root&password=";
         try {
-            Class.forName("com.mysql.jdbc.Driver");
+            /*Class.forName("com.mysql.jdbc.Driver");
             //String connectionUrl = "jdbc:mysql://localhost/mysql?" +"user=root&password=123456";
             connection = DriverManager.getConnection(prueba);
-            System.out.println("Conectado");
+            System.out.println("Conectado");*/
+
+            //nueva conexion
+            Class.forName("com.mysql.jdbc.Driver");
+		connection = DriverManager.getConnection(prueba);
+		DatabaseMetaData dbmd = connection.getMetaData();
+		System.out.println("Connection to " + dbmd.getDatabaseProductName()
+				+ " " + dbmd.getDatabaseProductVersion() + " successful.\n");
+
         } catch (SQLException e) {
             System.out.println("SQL Exception: "+ e.toString());
         } catch (ClassNotFoundException cE) {
