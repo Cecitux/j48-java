11dc9cd204022d8fed956041dd0d4f340de76af2
fercard06 <fercard06@892cf779-585f-cb9d-664d-18cd154d568d>
2010-11-06 16:05:25 +0000

    Separacion de funciones de bd

--- trunk/src/my/gui/DataBase.java
+++ trunk/src/my/gui/DataBase.java
@@ -8,26 +8,15 @@ import my.gui.gui;
 import java.sql.*;
 import java.util.*;
 
-//import java.sql.Connection;
-//import java.sql.DatabaseMetaData;
-//import java.sql.DriverManager;
-//import java.sql.ResultSet;
-//import java.sql.SQLException;
-//import java.sql.Statement;
-
-//import java.awt.*;
-//import java.awt.event.*;
-//import java.util.ArrayList;
-//import java.util.Iterator;
-
-
-
 
 public class DataBase {
     //Variables de Lore para la BD
     public static Connection connection;
     public static ArrayList data = new ArrayList();
-    public static String columna_desicion="risk";
+    public static String columna_decision="play";
+	public static String tabla_d = "";
+	//Los nombres de las columnas de la tabla
+	public static ArrayList nom_columnas = new ArrayList();
     //Conectar a la BD
     public static void Conectar(String nombre, String usuario, String pass) {
         // cadenaconexion = "";
@@ -40,15 +29,7 @@ public class DataBase {
             connection = DriverManager.getConnection(prueba);
             System.out.println("Conectado");
 
-            //nueva conexion
-            //Class.forName(gui.nombrebd+".mysql.jdbc.Driver");
-            //Ese "com" no se modifica
-            /*Class.forName("com.mysql.jdbc.Driver");
-		connection = DriverManager.getConnection(prueba);
-		DatabaseMetaData dbmd = connection.getMetaData();
-		System.out.println("Connection to " + dbmd.getDatabaseProductName()
-				+ " " + dbmd.getDatabaseProductVersion() + " successful.\n");*/
-
+          
         } catch (SQLException e) {
             System.out.println("SQL Exception: "+ e.toString());
         } catch (ClassNotFoundException cE) {
@@ -65,37 +46,56 @@ public class DataBase {
             System.out.println("Error al desconectar");
         }
     }
-
-    public static HashMap ParseoDB() throws SQLException{
-        HashMap hdb = new HashMap();
-        Statement stmt = null;
+	//Funcion que devuelve los distintos valores de la columna decision
+	public static ArrayList getValoresColDecision() throws SQLException{
+		Statement stmt = null;
         ResultSet rs = null;
-
-        ArrayList list = new ArrayList();
-        ArrayList nom_columnas = new ArrayList();
-        ArrayList queryactual = new ArrayList();
-        ArrayList val_col_desicion = new ArrayList();
-        ArrayList auxvalor = new ArrayList();
-        //nombre de la tabla de la BD, si hay mas de una se toma la primera
-        String tabla_d = "";
-        int numreg = 0, numvalcol=0;
-        String auxlista ="";
-        String auxcant = "";
-        String auxval = "", auxval1="";
-        String lineafila = "";
-        //SQL query command
-        String SQL = "show tables";
-        String SQLcantnum="";
-        String SQLcol="";
+		ArrayList lista = new ArrayList();
+		String SQL = "select "+columna_decision+" from "+tabla_d+" group by "+columna_decision;
+		stmt = connection.createStatement();
+        rs = stmt.executeQuery(SQL);
+		while (rs.next()) {
+            lista.add(rs.getString(columna_decision));
+        }
+		return lista;
+	}
+	//Funcion que devuelve los valores de las tablas para mostrarlos en pantalla
+	public static void getValoresPantalla()throws SQLException{
+		Statement stmt = null;
+        ResultSet rs = null;
+		//Esto no se usa, se debe hacer una funcion para traer los nombres de las columnas
+		
+		String lineafila = "";
+		 //Para mostrar en pantalla
+        String SQL = "select * from "+tabla_d;
         stmt = connection.createStatement();
         rs = stmt.executeQuery(SQL);
-        
+        Iterator it_nomcol = nom_columnas.iterator();
+        while (rs.next()) {
+            while (it_nomcol.hasNext()){
+                lineafila = lineafila + "\t"+ rs.getString(it_nomcol.next().toString());
+             }
+            lineafila = lineafila.substring(1);
+            lineafila = lineafila + "\t\n      ";
+            data.add(lineafila);
+            it_nomcol = nom_columnas.iterator();
+            lineafila = "";
+        }
+	}
+	//funcion que obitiene el nombre de la tabla y los nombres de las columnas
+	public static void getNombresColumnas()throws SQLException{
+		//SQL query command
+		Statement stmt = null;
+        ResultSet rs = null;
+        String SQL = "show tables";
+		ArrayList list = new ArrayList();
+        stmt = connection.createStatement();
+        rs = stmt.executeQuery(SQL);
         //Ver nombres de las tablas (se toma la primera)
         while (rs.next()) {
             list.add(rs.getString("Tables_in_com"));
         }
-        
-        tabla_d = list.get(2).toString();
+        tabla_d = list.get(4).toString();
         //Nombres de las columnas
         SQL = "describe "+tabla_d;
         stmt = connection.createStatement();
@@ -103,15 +103,27 @@ public class DataBase {
         while (rs.next()) {
             nom_columnas.add(rs.getString("Field"));
         }
-        //Para almacenar todos los valores del campo target
-        //val_col_desicion
-        SQL = "select "+columna_desicion+" from "+tabla_d+" group by "+columna_desicion;
-        stmt = connection.createStatement();
-        rs = stmt.executeQuery(SQL);
-        while (rs.next()) {
-            val_col_desicion.add(rs.getString(columna_desicion));
-        }
+	}
 
+    public static HashMap ParseoDB() throws SQLException{
+        HashMap hdb = new HashMap();
+        Statement stmt = null;
+        ResultSet rs = null;
+        //ArrayList nom_columnas = new ArrayList();
+        ArrayList queryactual = new ArrayList();
+        ArrayList val_col_desicion = new ArrayList();
+        ArrayList auxvalor = new ArrayList();
+        //nombre de la tabla de la BD, si hay mas de una se toma la primera
+        //String
+        int numreg = 0, numvalcol=0;
+        String auxlista ="";
+        //String auxcant = "";
+        String auxval = "", auxval1="";
+        String SQL = "";
+		String SQLcantnum="";
+        String SQLcol="";
+        
+		val_col_desicion = getValoresColDecision();
    //----------GUARDAR TODAS LAS VARIABLES DE UNA COLUMNA
         //auxlista = columnas
         //queryactual = distintos valores de las columnas
@@ -135,10 +147,9 @@ public class DataBase {
                 
             }
             HashMap hdbvalor_segundo_nivel = new HashMap();
-            if(!auxlista.contentEquals(columna_desicion)){
+            if(!auxlista.contentEquals(columna_decision)){
                 itval = queryactual.iterator();
                 //val_col_desicion
-                
                 while (itval.hasNext()){
                     //contar la cantidad de ocurrencias con respecto a la columna objetivo
                     HashMap hdbval = new HashMap();
@@ -146,9 +157,9 @@ public class DataBase {
                     itval1 = val_col_desicion.iterator();
                     while (itval1.hasNext()){
                         auxval1 = itval1.next().toString();
-                        SQLcol = "select "+auxlista+" from "+tabla_d+" where "+columna_desicion+" = '"+auxval1+"' and "+auxlista+"= '"+auxval+"'";
+                        SQLcol = "select "+auxlista+" from "+tabla_d+" where "+columna_decision+" = '"+auxval1+"' and "+auxlista+"= '"+auxval+"'";
 
-                        SQLcantnum = "select count("+auxlista+") from "+tabla_d+" where "+columna_desicion+" = '"+auxval1+"' and "+auxlista+"= '"+auxval+"'";
+                        SQLcantnum = "select count("+auxlista+") from "+tabla_d+" where "+columna_decision+" = '"+auxval1+"' and "+auxlista+"= '"+auxval+"'";
                         stmt = connection.createStatement();
                         rs = stmt.executeQuery(SQLcantnum);
                         while (rs.next()) {
@@ -175,21 +186,6 @@ public class DataBase {
                 System.out.println(s + " : " + s1);
           }
   
-         //Para mostrar en pantalla
-        SQL = "select * from "+tabla_d;
-        stmt = connection.createStatement();
-        rs = stmt.executeQuery(SQL);
-        it_nomcol = nom_columnas.iterator();
-        while (rs.next()) {
-            while (it_nomcol.hasNext()){
-                lineafila = lineafila + "\t"+ rs.getString(it_nomcol.next().toString());
-             }
-            lineafila = lineafila.substring(1);
-            lineafila = lineafila + "\t\n      ";
-            data.add(lineafila);
-            it_nomcol = nom_columnas.iterator();
-            lineafila = "";
-        }
         
         return hdb;
     }
